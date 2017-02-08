b259472d39c1d919be7eac1bce6fa9f25ffe478b
arkoblog <arkoblogr@gmail.com>
2017-02-08 21:13:51 +0545

    Responsive layout

--- app/components/Maps.js
+++ app/components/Maps.js
@@ -2,25 +2,16 @@ var React = require('react');
 var ReactDOM = require('react-dom')
 var L = require('leaflet')
 
-
 require('../styles/contents.css')
-
-var Popup = React.createClass({
-    render: function () {
-        return null
-    }
-})
-
-
 
 var LeafletMap = React.createClass({
     getInitialState: function() {
         var maxWindowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)-55;
         return {
-            height: maxWindowHeight
+            height: maxWindowHeight,
+            isLoading:false
         }
     },
-
     addPopup: function (d) {
         return ('<strong>Name: </strong>'+d.name
                 +'<br/><strong>ICU: </strong>'+d["facility:icu"] 
@@ -39,11 +30,11 @@ var LeafletMap = React.createClass({
         L.tileLayer('https://api.mapbox.com/styles/v1/arkoblog/ciy2j6jja00g52sqdi7u4114x/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJrb2Jsb2ciLCJhIjoiY2l5MmczdzJyMDAxODJxcDY5NHMyeHpkMyJ9.la6WiYXrUzF1Iy4aST9tnA', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           }).addTo(map);
-         
+
     },
 
     addMarkers:function(data) {
-        markerLayer =new L.featureGroup;
+        markerLayer = new L.featureGroup;
         data.features.map(function(d){
           var marker = new L.marker([d.geometry.coordinates[1], d.geometry.coordinates[0]]).addTo(markerLayer)
           .bindPopup(this.addPopup(d.properties.tags))
@@ -52,14 +43,16 @@ var LeafletMap = React.createClass({
     },
 
     updateMarkers:function(data) {
+
         this.map.removeLayer(markerLayer)
         markerLayer = new L.featureGroup;
         data.features.map(function(d){
           var marker = new L.marker([d.geometry.coordinates[1], d.geometry.coordinates[0]]).addTo(markerLayer)
-          .bindPopup(this.popup(d.properties.tags))
+          .bindPopup(this.addPopup(d.properties.tags))
           // .bindPopup('<strong>Name</strong><br>'+d.properties.tags.name)
         }.bind(this));
         markerLayer.addTo(this.map);
+
     },
 
     updateDimensions: function() {
@@ -69,21 +62,26 @@ var LeafletMap = React.createClass({
         })
     },
 
+
     componentDidMount: function () {
         this.rendermap();
         this.addMarkers(this.props.data);
         window.addEventListener("resize", this.updateDimensions);
     },
 
+
     componentDidUpdate: function () {
         this.updateMarkers(this.props.data);
     },
-    
-    render: function() {
 
-      return(
-          <div id="map" style={{height:this.state.height}}></div> 
-        )
+    render: function() {
+            return(
+                        <div>
+                        <div id="map" style={{height:this.state.height}}>
+                        </div> 
+                        </div> 
+                    )
+
     }
 })
 
